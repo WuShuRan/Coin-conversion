cca38832dcc783f24813cc1d62d7dceac702ee25
wushuran <wushuran2017@gmail.com>
2017-07-14 10:15:41 +0800

    2017-07-14 修改了老师让修改的http请求问题，即修改发送http请求的方式（从HttpClient修改为HttpURLConnection）

--- MyCurrencies/app/src/main/java/com/zucc/wushuran31401394/mycurrencies/JSONParser.java
+++ MyCurrencies/app/src/main/java/com/zucc/wushuran31401394/mycurrencies/JSONParser.java
@@ -5,11 +5,6 @@ package com.zucc.wushuran31401394.mycurrencies;
  */
 import android.util.Log;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -18,54 +13,66 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class JSONParser {
     static InputStream sInputStream = null;
     static JSONObject sReturnJsonObject = null;
     static String sRawJsonString = "";
-    public JSONParser() {}
+    int code;
+
+    public JSONParser() {
+    }
+
     public JSONObject getJSONFromUrl(String url) {
         //尝试从服务器获取响应
-        Log.d("JSONParser",url);
+        Log.d("JSONParser", url);
         try {
             //http客户端
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            //获取http的内容
-            sInputStream = httpEntity.getContent();
+            URL myUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) myUrl
+                    .openConnection();
+            conn.setRequestMethod("GET");//使用GET方法获取
+            conn.setReadTimeout(8000);
+            conn.setConnectTimeout(8000);
+            code = conn.getResponseCode();
+            Log.d("Code", String.valueOf(code));
+            if (code == 200) {
+                sInputStream = conn.getInputStream();
+                //在数据流里读取内容
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(
+                            sInputStream, "iso-8859-1"), 8);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        //一行一行读取变成一个整体
+                        stringBuilder.append(line + "\n");
+                    }
+                    sInputStream.close();
+                    //辅助
+                    sRawJsonString = stringBuilder.toString();
+                    Log.d("result", sRawJsonString);
+                } catch (Exception e) {
+                    Log.e("Error reading from: " + e.toString(), this.getClass().getSimpleName());
+                }
+                try {
+                    //解析
+                    sReturnJsonObject = new JSONObject(sRawJsonString);
+                    Log.d("JSON", sReturnJsonObject.getString("disclaimer"));
+                } catch (JSONException e) {
+                    Log.e("Parser", "Error when parsing data " + e.toString());
+                }
+                //return json object
+                conn.disconnect();
+                return sReturnJsonObject;
+            }
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //在数据流里读取内容
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    sInputStream, "iso-8859-1"), 8);
-            StringBuilder stringBuilder = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                //一行一行读取变成一个整体
-                stringBuilder.append(line + "\n");
-            }
-            sInputStream.close();
-            //辅助
-            sRawJsonString = stringBuilder.toString();
-            Log.d("result",sRawJsonString);
-        } catch (Exception e) {
-            Log.e("Error reading from: " + e.toString(), this.getClass().getSimpleName());
-        }
-        try {
-            //解析
-            sReturnJsonObject = new JSONObject(sRawJsonString);
-            Log.d("JSON",sReturnJsonObject.getString("disclaimer"));
-        } catch (JSONException e) {
-            Log.e("Parser", "Error when parsing data " + e.toString());
-        }
-        //return json object
-        return sReturnJsonObject;
+        return null;
     }
 }
